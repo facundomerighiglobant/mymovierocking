8793a30dc0b390941bc5689f5b80527115023180
Facundo Merighi <fmerigh@searshc.com>
2015-11-13 11:33:57 -0300

    movie proyect

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mymovierockingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mymovierockingApp
+ */
+angular.module('mymovierockingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mymovierockingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mymovierockingApp
+ */
+angular.module('mymovierockingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.test = 'testin';
+  });
